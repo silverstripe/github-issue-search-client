03a7f863dc6b68b7ec4c2b6bcadfdfac93114ea2
Tim Kung <tkung@silverstripe.com>
2018-03-16 12:18:48 +1300

    moving search result into own component and surfacing more data in the template

---
+++ src/components/SearchResult.vue
@@ -0,0 +1,49 @@
+<template>
+  <li class="result">
+    <h3 class="result__title">
+      <a :href="issueData.url" target="_blank">
+        {{ issueData.title }}
+      </a>
+    </h3>
+    <dl>
+      <dt>Status</dt>
+      <dd>{{ issueData.state }}</dd>
+      <dt>Created</dt>
+      <dd>{{ issueData.createdAt }}</dd>
+      <dt>Repository</dt>
+      <dd><a :href="issueData.repository.url" target="_blank">{{ issueData.repository.name }}</a></dd>
+    </dl>
+  </li>
+</template>
+
+<script>
+  /**
+   * Data mockup:
+   *
+   * createdAt: "2018-03-15T14:08:41Z"
+   * id: "MDU6SXNzdWUzMDU1NjczNzU="
+   * labels: {nodes: [], __typename: "LabelConnection"}
+   * state: "OPEN"
+   * title: "[Recoverable Error] Argument 1 passed to SilverStripe\Config\MergeStrategy\Priority::mergeArray() must be of the type array"
+   * url: "https://github.com/silverstripe/silverstripe-framework/issues/7938"
+   */
+  export default {
+    props: {
+      issueData: {
+        type: Object,
+      },
+    },
+
+    data() {
+      return {};
+    },
+
+    computed: {
+
+    },
+
+    methods: {
+
+    },
+  }
+</script>
--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -24,9 +24,7 @@
         <!-- Result -->
         <div v-else-if="data" class="result apollo">
           <ul>
-            <li v-bind:key="issue.id" v-for="issue in data.search.nodes">
-              {{ issue.title }}
-            </li>
+            <SearchResult v-for="issue in data.search.nodes" :key="issue.id" :issue-data="issue" />
           </ul>
         </div>
 
@@ -39,41 +37,50 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      query: ""
-    };
-  },
+  import SearchResult from './SearchResult';
 
-  computed: {
-    compositeQuery() {
-      return `${
-        this.query
-      } is:open is:issue repo:silverstripe/silverstripe-framework repo:silverstripe/silverstripe-cms repo:silverstripe/silverstripe-admin repo:silverstripe/silverstripe-installer repo:silverstripe/silverstripe-asset-admin repo:silverstripe/silverstripe-versioned repo:silverstripe/silverstripe-reports repo:silverstripe/silverstripe-siteconfig repo:silverstripe/silverstripe-assets repo:silverstripe/silverstripe-campaign-admin repo:silverstripe/silverstripe-errorpage repo:silverstripe/silverstripe-graphql repo:silverstripe/recipe-core repo:silverstripe/recipe-plugin repo:silverstripe/recipe-cms`;
-    }
-  },
+  export default {
+    data() {
+      return {
+        query: ""
+      };
+    },
 
-  methods: {}
-};
+    components: {
+      SearchResult,
+    },
+
+    computed: {
+      compositeQuery() {
+        return `
+          ${this.query}
+          is:open
+          is:issue
+          repo:silverstripe/silverstripe-framework repo:silverstripe/silverstripe-cms repo:silverstripe/silverstripe-admin repo:silverstripe/silverstripe-installer repo:silverstripe/silverstripe-asset-admin repo:silverstripe/silverstripe-versioned repo:silverstripe/silverstripe-reports repo:silverstripe/silverstripe-siteconfig repo:silverstripe/silverstripe-assets repo:silverstripe/silverstripe-campaign-admin repo:silverstripe/silverstripe-errorpage repo:silverstripe/silverstripe-graphql repo:silverstripe/recipe-core repo:silverstripe/recipe-plugin repo:silverstripe/recipe-cms
+        `;
+      }
+    },
+
+    methods: {}
+  };
 </script>
 
 <style scoped>
-.form,
-.input,
-.apollo,
-.message {
-  padding: 12px;
-}
+  .form,
+  .input,
+  .apollo,
+  .message {
+    padding: 12px;
+  }
 
-.input {
-  font-family: inherit;
-  font-size: inherit;
-  border: solid 2px #ccc;
-  border-radius: 3px;
-}
+  .input {
+    font-family: inherit;
+    font-size: inherit;
+    border: solid 2px #ccc;
+    border-radius: 3px;
+  }
 
-.error {
-  color: red;
-}
+  .error {
+    color: red;
+  }
 </style>
