6886e8c606ced295a3de1b23d630c4106f71f27a
Ingo Schommer <me@chillu.com>
2018-03-16 06:29:08 +1300

    Boilerplate

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { apolloProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
 
 new Vue({
+  provide: apolloProvider.provide(),
   render: h => h(App)
 }).$mount('#app')
