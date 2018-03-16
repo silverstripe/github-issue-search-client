32a0cf9e669fbfce2c7c370dcfe1ffda3b0315d5
Tim Kung <tkung@silverstripe.com>
2018-03-16 14:33:37 +1300

    adding labels to each issue

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -5,7 +5,7 @@
         {{ issueData.title }}
       </a>
     </h3>
-    <dl>
+    <dl class="result__details">
       <dt>Status</dt>
       <dd>{{ issueData.state }}</dd>
       <dt>Created</dt>
@@ -13,6 +13,16 @@
       <dt>Repository</dt>
       <dd><a :href="issueData.repository.url" target="_blank">{{ issueData.repository.name }}</a></dd>
     </dl>
+    <div class="result__labels">
+      <span
+        v-for="label in issueData.labels.nodes"
+        :key="label.id"
+        :style="{ borderColor: `#${label.color}` }"
+        class="result__label"
+      >
+        {{ label.name }}
+      </span>
+    </div>
   </li>
 </template>
 
@@ -20,11 +30,17 @@
   /**
    * Data mockup:
    *
-   * createdAt: "2018-03-15T14:08:41Z"
-   * id: "MDU6SXNzdWUzMDU1NjczNzU="
-   * labels: {nodes: [], __typename: "LabelConnection"}
-   * state: "OPEN"
-   * title: "[Recoverable Error] Argument 1 passed to SilverStripe\Config\MergeStrategy\Priority::mergeArray() must be of the type array"
+   * createdAt: "2018-03-15T14:08:41Z",
+   * id: "MDU6SXNzdWUzMDU1NjczNzU=",
+   * labels: {
+   *   nodes: [
+   *     0: {id: "MDU6TGFiZWwzODYzMTYwNDE=", name: "affects/v4", color: "5319e7", __typename: "Label"},
+   *     ...
+   *   ],
+   *   __typename: "LabelConnection"
+   * },
+   * state: "OPEN",
+   * title: "[Recoverable Error] Argument 1 passed to SilverStripe\Config\MergeStrategy\Priority::mergeArray() must be of the type array",
    * url: "https://github.com/silverstripe/silverstripe-framework/issues/7938"
    */
   export default {
@@ -47,3 +63,14 @@
     },
   }
 </script>
+
+<style scoped>
+  .result__label {
+    border-style: solid;
+    border-width: 1px;
+    display: inline-block;
+    font-weight: 700;
+    margin: 5px;
+    padding: 5px 10px;
+  }
+</style>
