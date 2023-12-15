353610e4517c17602792d481a8b10872395c0ce9
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2023-12-15 13:24:49 +1300

    NEW Add "community contributions only" checkbox

--- src/types.d.ts
+++ src/types.d.ts
@@ -14,6 +14,7 @@ export type FormData = {
   mode: Mode,
   customRepos: string[],
   includeSupported: boolean,
+  communityOnly: boolean,
   issueStatus: IssueStatus,
   issueType: IssueType,
   sort: Sort,
